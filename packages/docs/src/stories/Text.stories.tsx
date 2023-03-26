e668cded02830fd8f1d639296520583d59268726
Lucas Roseti <lc.roseti@gmail.com>
2023-03-26 17:50:20 -0300

    Components
      - Box, Text, Heading
      - Avatar, Button, TextInput, TextArea
      - Checkbox, MultiStep
      - Phosphor react

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptas aliquid quo vitae. Distinctio itaque officiis, laborum recusandae dignissimos nulla fugit cumque ullam laboriosam rem, perspiciatis minima error sit? Nemo.',
+    size: 'md',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
